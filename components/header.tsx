57bc41f91c4cce5630b7047f3b19d4c3023d1abd
prettydev <creditcoder@hotmail.com>
2020-11-18 15:35:28 -0800

    fix style

--- components/header.tsx
+++ components/header.tsx
@@ -1,46 +1,51 @@
-import Link from 'next/link'
-import { signIn, signOut, useSession } from 'next-auth/client'
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 const Header = () => {
-  const [ session, loading ] = useSession()
-  
+  const [session, loading] = useSession();
+
   return (
     <header>
       <div>
         <p>
-          {!session && <div className="flex flex-row justify-between items-center">
-            <span >You are not signed in</span>
-            <a
-                className="bg-blue-500 p-3 rounded-lg"
+          {!session && (
+            <div className="flex flex-row justify-between items-center">
+              <span>You are not signed in</span>
+              <a
+                className="bg-blue-500 p-3 rounded-lg text-white"
                 href={`/api/auth/signin`}
                 onClick={(e) => {
-                  e.preventDefault()
-                  signIn("facebook")
+                  e.preventDefault();
+                  signIn("facebook");
                 }}
               >
                 Sign in with facebook
               </a>
-          </div>}
-          {session && <div className="flex flex-row justify-between items-center">
-            <span className="flex flex-row gap-2">
-              <small>Signed in as</small>
-              <strong>{session.user.email || session.user.name}</strong>
+            </div>
+          )}
+          {session && (
+            <div className="flex flex-row justify-between items-center">
+              <span>
+                <small>Signed in as</small>
+                <br />
+                <strong>{session.user.email || session.user.name}</strong>
               </span>
-            <a
-                className="bg-blue-500 p-3 rounded-lg"
+              <a
+                className="bg-blue-500 p-3 rounded-lg text-white"
                 href={`/api/auth/signout`}
                 onClick={(e) => {
-                  e.preventDefault()
-                  signOut()
+                  e.preventDefault();
+                  signOut();
                 }}
               >
                 Sign out
               </a>
-          </div>}
+            </div>
+          )}
         </p>
-      </div>      
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -44,13 +44,13 @@ const Page = () => {
       {session && (
         <table className="w-full table-auto">
           <thead className="justify-between">
-            <div className="bg-gray-800 px-16 py-2 flex flex-row justify-between">
+            <div className="bg-gray-800 px-8 py-2 flex flex-row justify-between">
               <span className="text-gray-300">Field</span>
               <span className="text-gray-300">Value</span>
             </div>
           </thead>
           <tbody className="bg-gray-200">
-            <div className="border-4 border-gray-200 flex flex-row items-center px-16 py-2 justify-between">
+            <div className="border-4 border-gray-200 flex flex-row items-center px-8 py-2 justify-between">
               <div>image</div>
               <img
                 className="h-8 w-8 rounded-full object-cover "
@@ -59,19 +59,19 @@ const Page = () => {
                 title={session.user.image}
               />
             </div>
-            <div className="border-4 border-gray-200 flex flex-row items-center px-16 py-2 justify-between">
+            <div className="border-4 border-gray-200 flex flex-row items-center px-8 py-2 justify-between">
               <div>Name</div>
               <div className="text-center ml-2 font-semibold">
                 {session.user.name}
               </div>
             </div>
-            <div className="border-4 border-gray-200 flex flex-row items-center px-16 py-2 justify-between">
+            <div className="border-4 border-gray-200 flex flex-row items-center px-8 py-2 justify-between">
               <div>Email</div>
               <div className="text-center ml-2 font-semibold">
                 {session.user.email}
               </div>
             </div>
-            <div className="border-4 border-gray-200 flex flex-row items-center px-16 py-2 justify-between">
+            <div className="border-4 border-gray-200 flex flex-row items-center px-8 py-2 justify-between">
               <div>Expires</div>
               <div>{session.expires}</div>
             </div>
